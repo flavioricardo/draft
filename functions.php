184fcedbe4a63bd3bd4be458f936caec16918739
Flávio Ricardo <flavioricardo@live.com>
2018-12-11 13:01:16 -0200

    Customizing Gutenberg editor CSS

--- functions.php
+++ functions.php
@@ -23,6 +23,8 @@ if ( function_exists( 'add_theme_support' ) )
 
     // Enable Gutenberg Support
     add_theme_support( 'align-wide' );
+    add_theme_support( 'editor-styles' );
+    add_editor_style( 'style-editor.css' );
 
     // Enable Thumbnail Support
     add_theme_support( 'post-thumbnails' );
