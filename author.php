85af113949f11930ebc829800d5f39c660b960bd
Flávio Ricardo <flavioricardo@live.com>
2019-03-07 22:50:06 -0300

    New author and tag archive

---
+++ author.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div class="col-8">
+
+		<div class="card mb-4">
+			<div class="card-body">
+				<h3><?php echo get_the_author_meta( 'display_name' ); ?></h3>
+				<?php the_archive_description(); ?>
+			</div>
+		</div>
+
+		<?php if ( have_posts() ) : ?>
+			<?php while  ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'templates/content', get_post_format() ); ?>
+
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+
+			<div class="navigation">
+				<?php wp_pagenavi(); ?>
+			</div>
+
+		<?php else : ?>
+			<?php _e( 'Sorry, no posts matched your criteria.', 'draft' ); ?>
+		<?php endif; ?>
+
+	</div>
+
+	<?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ tag.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div class="col-8">
+
+		<div class="card mb-4">
+			<div class="card-body">
+				<h3><?php single_tag_title(); ?></h3>
+				<?php the_archive_description(); ?>
+			</div>
+		</div>
+
+		<?php if ( have_posts() ) : ?>
+			<?php while  ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'templates/content', get_post_format() ); ?>
+
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+
+			<div class="navigation">
+				<?php wp_pagenavi(); ?>
+			</div>
+
+		<?php else : ?>
+			<?php _e( 'Sorry, no posts matched your criteria.', 'draft' ); ?>
+		<?php endif; ?>
+
+	</div>
+
+	<?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
