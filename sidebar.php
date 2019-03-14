c2f653da9f597656c7f43966c8575a48fc8b2ab4
Flávio Ricardo <flavioricardo@live.com>
2019-03-14 18:18:56 -0300

    New esports schedule

--- sidebar.php
+++ sidebar.php
@@ -41,13 +41,13 @@
 	<div class="sidebar-widget mb-4">
 		<?php
 		$current_post = 0;
-		$the_query = new WP_Query( array( 'category__in' => 25, 'posts_per_page' => 5 ) );
-		if ( $the_query->have_posts() ) : ?>
+		$analises = new WP_Query( array( 'category__in' => 25, 'posts_per_page' => 5 ) );
+		if ( $analises->have_posts() ) : ?>
 			<div class="section-call">
 				<h3>Últimas <strong>Análises</strong></h3>
 			</div>
 			<ul class="list-group shadow-sm">
-			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+			<?php while ( $analises->have_posts() ) : $analises->the_post(); ?>
 				<?php if ($current_post == 0) : ?>
 					<li class="list-group-item list-group-item-action rounded-0">
 						<a class="card-link" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -70,6 +70,51 @@
 		<?php endif;
 		wp_reset_postdata();
 		?>
+	</div>
+
+	<div class="sidebar-widget mb-4">
+		<div class="section-call">
+			<h3>Calendário <strong>eSports</strong></h3>
+		</div>
+		<ul class="nav nav-pills" role="tablist">
+			<?php
+			$campeonatos = get_terms( array( 'taxonomy' => 'campeonatos', 'childless' => true ) );
+			foreach ($campeonatos as $campeonato) : ?>
+				<li class="nav-item">
+					<a class="nav-link" id="<?php echo $campeonato->slug; ?>-tab" data-toggle="tab" href="#<?php echo $campeonato->slug; ?>" role="tab" aria-controls="<?php echo $campeonato->slug; ?>" aria-selected="true"><?php echo $campeonato->name; ?></a>
+				</li>
+			<?php
+			endforeach; ?>
+		</ul>
+		<div class="tab-content">
+			<?php
+			$campeonatos = get_terms( array( 'taxonomy' => 'campeonatos', 'childless' => true ) );
+			foreach ($campeonatos as $campeonato) : ?>
+				<div class="tab-pane fade" id="<?php echo $campeonato->slug; ?>" role="tabpanel" aria-labelledby="<?php echo $campeonato->slug; ?>-tab">
+					<?php
+					$partidas = new WP_Query( array( 'posts_per_page' => 5, 'post_type' => 'partidas',
+					'tax_query' => array( array ( 'taxonomy' => 'campeonatos', 'field' => 'slug', 'terms' => $campeonato->slug ) ) ) );
+					if ( $partidas->have_posts() ) :
+						while ( $partidas->have_posts() ) : $partidas->the_post(); ?>
+							<div class="row">
+								<div class="col-5">
+									<img src="<?php the_field('time_casa'); ?>" class="img-fluid" alt="">
+								</div>
+								<div class="col-2">
+									<?php the_field('etapa'); ?>
+									<?php the_field('data_e_hora'); ?>
+								</div>
+								<div class="col-5">
+									<img src="<?php the_field('time_fora'); ?>" class="img-fluid" alt="">
+								</div>
+							</div>
+						<?php
+						endwhile;
+					endif; ?>
+				</div>
+			<?php
+			endforeach; ?>
+		</div>
 	</div>
 
 	<div class="sidebar-widget mb-4">
