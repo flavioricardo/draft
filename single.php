b49390fbf8b98093d6e1f99754eb9abc74f735c6
Flávio Ricardo <flavioricardo@live.com>
2019-11-15 17:01:34 -0300

    Classe do gravatar

--- single.php
+++ single.php
@@ -4,12 +4,12 @@
 
 	<div class="col-sm-12 col-lg-8">
 
-		<?php if ( have_posts() ) : ?>
-			<?php while  ( have_posts() ) : the_post(); ?>
+		<?php if (have_posts()) : ?>
+			<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'templates/content', 'single' ); ?>
+				<?php get_template_part('templates/content', 'single'); ?>
 
-				<?php if ( comments_open() || get_comments_number() ) : ?>
+				<?php if (comments_open() || get_comments_number()) : ?>
 					<?php comments_template(); ?>
 				<?php endif; ?>
 
@@ -21,12 +21,12 @@
 			</div>
 
 		<?php else : ?>
-			<?php _e( 'Sorry, no posts matched your criteria.', 'draft' ); ?>
+			<?php _e('Sorry, no posts matched your criteria.', 'draft'); ?>
 		<?php endif; ?>
 
 	</div>
 
-	<?php get_sidebar( 'sidebar-2' ); ?>
+	<?php get_sidebar('sidebar-2'); ?>
 
 </div>
 
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,15 +1,15 @@
-<article <?php post_class( 'blog-post' ); ?>>
+<article <?php post_class('blog-post'); ?>>
 	<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
 	<div class="blog-post-meta">
 		<div class="row mb-3">
 			<div class="col-6 d-none d-lg-inline-block">
 				<i class="fas fa-calendar-alt d-none d-lg-inline-block"></i>
-				<div class="d-inline-block">Por <?php the_author_posts_link(); ?> em <?php the_time( 'd/m/Y' ); ?></div>
+				<div class="d-inline-block">Por <?php the_author_posts_link(); ?> em <?php the_time('d/m/Y'); ?></div>
 			</div>
 			<div class="col-6 d-none d-lg-inline-block text-right">
 				<i class="fas fa-tags d-none d-lg-inline-block"></i>
-				<div class="d-inline-block"><?php the_category( ' ' ); ?></div>
+				<div class="d-inline-block"><?php the_category(' '); ?></div>
 			</div>
 		</div>
 	</div>
@@ -18,13 +18,13 @@
 		<?php the_excerpt(); ?>
 	</div>
 
-	<?php if ( has_post_thumbnail() && !get_post_format() ) : ?>
+	<?php if (has_post_thumbnail() && !get_post_format()) : ?>
 		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-			<?php the_post_thumbnail( 'large', array('class' => 'img-fluid flex-auto mb-3') ); ?>
+			<?php the_post_thumbnail('large', array('class' => 'img-fluid flex-auto mb-3')); ?>
 		</a>
 	<?php endif; ?>
 
-	<?php if ( get_post_format() && get_post_format() == 'video' ) : ?>
+	<?php if (get_post_format() && get_post_format() == 'video') : ?>
 		<div class="d-block mt-3"></div>
 	<?php endif; ?>
 
@@ -34,18 +34,18 @@
 	<div class="row">
 		<div class="col-lg-9">
 			<i class="fas fa-bookmark d-none d-lg-inline-block"></i>
-			<div class="d-inline"><?php the_tags( '', ', ', '' ); ?></div>
+			<div class="d-inline"><?php the_tags('', ', ', ''); ?></div>
 		</div>
 
-		<?php do_action( 'after_article' ); ?>
+		<?php do_action('after_article'); ?>
 	</div>
 
 	<div class="row d-none d-lg-flex mb-4 shadow-sm pt-2 pb-3 align-middle">
 		<div class="col-lg-2 mt-2">
-			<?php echo get_avatar(get_the_author_meta('ID'), 90, $default = '', get_the_author_meta('display_name'), array('class' => 'avatar')); ?>
+			<?php echo get_avatar(get_the_author_meta('ID'), 90, $default = '', get_the_author_meta('display_name'), array('class' => array('avatar'))); ?>
 		</div>
 		<div class="col-lg-10">
-			<?php $display_name = explode( ' ', get_the_author_meta('display_name') ); ?>
+			<?php $display_name = explode(' ', get_the_author_meta('display_name')); ?>
 			<div class="text-left"><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php echo $display_name[0]; ?> <strong><?php echo $display_name[1]; ?></strong></a></div>
 			<div class="text-left font-italic"><?php echo get_the_author_meta('description'); ?></div>
 		</div>
