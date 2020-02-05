5eee87e919c66ee81306a720a1ab87aefa7985ca
Flávio Ricardo <flavioricardo@live.com>
2020-02-05 19:04:30 -0300

    Darkmode

--- footer.php
+++ footer.php
@@ -12,7 +12,20 @@
 
 	<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.4/lib/darkmode-js.min.js"></script>
 	<script>
-	new Darkmode().showWidget();
+		var options = {
+			bottom: '64px', // default: '32px'
+			right: 'unset', // default: '32px'
+			left: '32px', // default: 'unset'
+			time: '0.5s', // default: '0.3s'
+			mixColor: '#fff', // default: '#fff'
+			backgroundColor: '#fff',  // default: '#fff'
+			buttonColorDark: '#100f2c',  // default: '#100f2c'
+			buttonColorLight: '#fff', // default: '#fff'
+			saveInCookies: false, // default: true,
+			label: '🌓', // default: ''
+			autoMatchOsTheme: true // default: true
+		};
+		new Darkmode(options).showWidget();
 	</script>
 
 	<?php wp_footer(); ?>
