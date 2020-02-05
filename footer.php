bb7a7d17c8e40bf96479ae2b19637c117edc694b
Flávio Ricardo <flavioricardo@live.com>
2020-02-05 19:06:23 -0300

    Darkmode

--- footer.php
+++ footer.php
@@ -13,17 +13,17 @@
 	<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.4/lib/darkmode-js.min.js"></script>
 	<script>
 		var options = {
-			bottom: '64px', // default: '32px'
-			right: 'unset', // default: '32px'
-			left: '32px', // default: 'unset'
-			time: '0.5s', // default: '0.3s'
+			// bottom: '64px', // default: '32px'
+			// right: 'unset', // default: '32px'
+			// left: '32px', // default: 'unset'
+			// time: '0.5s', // default: '0.3s'
 			mixColor: '#fff', // default: '#fff'
 			backgroundColor: '#fff',  // default: '#fff'
 			buttonColorDark: '#100f2c',  // default: '#100f2c'
 			buttonColorLight: '#fff', // default: '#fff'
-			saveInCookies: false, // default: true,
+			// saveInCookies: false, // default: true,
 			label: '🌓', // default: ''
-			autoMatchOsTheme: true // default: true
+			// autoMatchOsTheme: true // default: true
 		};
 		new Darkmode(options).showWidget();
 	</script>
